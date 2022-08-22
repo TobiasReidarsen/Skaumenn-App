e539fa5a2582421d82a25c023a8c71e26299ab8a
WinstonSmith314 <tobias.reidarsen@gmail.com>
2022-08-22 10:07:00 +0200

    Update mapView.js

--- JS/mapView.js
+++ JS/mapView.js
@@ -8,7 +8,9 @@ function mapView() {
     app = document.getElementById("app");
     let html = /*HTML*/``;
 
-    
+    html += /*HTML*/ `
+        <div></div>
+    `;
 
     app.innerHTML = html;
 }
