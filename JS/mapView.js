668961c17a110304b5fd5c0c17afb242a5153bc3
WinstonSmith314 <tobias.reidarsen@gmail.com>
2022-08-22 17:11:37 +0200

    asdasdasd
    asdasd

--- JS/mapView.js
+++ JS/mapView.js
@@ -1,17 +1,14 @@
-// Initialize and add the map
-function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
-  });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
+window.onload = function () {
+    view();
 }
 
-window.initMap = initMap;
+function view(){
+    let app = document.getElementById("app");
+    let html = /*HTML*/``;
+
+    html += /*HTML*/`
+        <input type="text" />
+    `;
+    console.log("laster");
+    app.innerHTML = html;
+}
